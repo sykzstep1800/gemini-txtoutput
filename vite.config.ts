ed853a2e93cf9c8b1de04ecc043cb4387b577e89
ユーザー名 <>
2025-09-04 03:35:34 +0900

    base add

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/gemini-txtoutput/",
   plugins: [react(), tailwindcss()],
 });
